946f7b821fe83029c3715b0412c8395443e5e7bd
ibsalim <ibsalim073@gmail.com>
2023-03-17 13:26:40 +0500

    Thumbnail size update

--- src/components/views/movies/MoviesList.vue
+++ src/components/views/movies/MoviesList.vue
@@ -2,7 +2,7 @@
     <div class="bg-grey-darkest">
         <div class="font-bold mb-4 mt-5 text-2xl text-center text-teal-dark">{{ moduleTitle }}</div>
         <ul class="flex flex-wrap justify-center px-12">
-            <li v-for="movie in movies" :key="movie.id" style="list-style: none;" class="bg-blue-darkest border border-blue-darker px-4 py-2 text-teal lg:w-1/5 sm:w-1/3 mx-2 my-2">
+            <li v-for="movie in movies" :key="movie.id" style="list-style: none;" class="bg-blue-darkest border border-blue-darker px-4 py-2 text-teal lg:w-1/6 sm:w-1/3 mx-2 my-2">
                 <list-item v-bind:movie="movie"></list-item>
             </li>
         </ul>
